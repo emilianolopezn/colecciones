43a2d49cd1d4e7a2f1a1207ce0a9395561568b86
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-23 08:09:36 -0700

    Agregar archivos de proyecto.

---
+++ Colecciones/Materia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Materia
+    {
+        public string Nombre { get; set; }
+        public string Identificador { get; set; }
+
+        public Materia()
+        {
+            Nombre = "Materia nueva";
+            Identificador = "0";
+        }
+
+        public Materia(string nombre, 
+            string identificador)
+        {
+            Nombre = nombre;
+            Identificador = identificador;
+        }
+
+    }
+}
---
+++ Colecciones/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Declaracion e inicializacion de 
+            //lista de alumos
+            List<Alumno> alumnos =
+                new List<Alumno>();
+
+            //Declaracion y asignacion de valores
+            // de alumno1
+            Alumno alumno1 =
+                new Alumno();
+            alumno1.Nombre = "Jose Perez";
+            alumno1.Materias.Add(
+                new Materia("Historia", "HST741"));
+            alumno1.Materias.Add(
+                new Materia("Matematicas", "MAT789"));
+            alumno1.Materias.Add(
+                new Materia("Civismo", "CVC753"));
+
+
+            alumnos.Add(alumno1);
+
+        }
+    }
+}
